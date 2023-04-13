70f0808bf2f54667c1c2a4b47cc793f6eb156c5b
davidinmichael <davidinmichael@gmail.com>
2023-04-13 12:06:49 +0100

    I worked on the login system. Users can now login successfully

--- account/views.py
+++ account/views.py
@@ -20,7 +20,7 @@ def register(request):
         if password == confirm_password:
             user, created = User.objects.get_or_create(email=email, username = username, defaults={'first_name': first_name, 'last_name': last_name, 'password': password})
             login(request, user)
-            return redirect('home')
+            return redirect('login')
         
         # throw and error message if a user tries to create an account with an existing mail in database
         messages.error(request, 'User with email already exists')
@@ -30,5 +30,18 @@ def register(request):
         return render(request, "account/register.html", context)
 
 def login_user(request):
-    context = {}
-    return render(request, "account/login.html", context)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # messages.success(request, "{{username}}, Login is succesfull")
+            return redirect("home")
+        else:
+            messages.error(request, "User does not exist, please Register.")
+            return redirect("register")
+    
+    else:
+        context = {}
+        return render(request, "account/login.html", context)
