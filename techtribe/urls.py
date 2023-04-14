933a85972b1ba72a516b85a1d9a42c64ffaca561
davidinmichael <davidinmichael@gmail.com>
2023-04-14 14:26:51 +0100

    I set the nav-bar links to redirect to the correct pages.

--- techtribe/urls.py
+++ techtribe/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("account.urls")),
+    path("account/", include("account.urls")),
     path("blog/", include("blog.urls")),
 ]
