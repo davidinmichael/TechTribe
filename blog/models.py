7a91d0c50c436c2e2c606874ec67f0e73a3748c3
davidinmichael <davidinmichael@gmail.com>
2023-04-14 17:31:41 +0100

    I created the blog post model and configured it with the database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='uploads/')
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def blog(request):
-    return render(request, "blog/blog.html")
+    articles = Article.objects.all()
+    context = {
+        "articles" : articles,
+    }
+    return render(request, "blog/blog.html", context)
